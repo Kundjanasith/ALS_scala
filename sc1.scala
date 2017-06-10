d68d10c885580f23ca93aebffa6212cee64e7c06
Kjzth <kjzth@Kundjanasiths-MacBook-Air.local>
2017-06-11 03:00:36 +0900

    star debug

--- sc1.scala
+++ sc1.scala
@@ -12,6 +12,17 @@ import scala.collection.mutable.ListBuffer
 // movies = pd.read_table('data/movies.dat',sep='::', header=None, names=movie_headers)
 // movie_titles = movies.title.tolist()
 
+def print_readable( arr: ListBuffer[Array[Double]] ){
+    print("[")
+    for( i <- 0 to arr.length - 1 ){
+        print("(")
+        for( j <- 0 to arr.apply(0).length - 1 ){
+            print( arr.apply(i).apply(j) + ":" )
+        }
+        print(")")
+    }
+    print("]")
+}
 val filename = "data/rat.csv"
 val users = new ListBuffer[Int]()
 val movies = new ListBuffer[Int]()
@@ -41,7 +52,13 @@ for (line <- Source.fromFile(filename).getLines()) {
     ratings += temp
   }
 }
-var ratingArr = ratings.to[Array]
+
+val lambda_ = 0.1
+val n_factors = 10
+val m = ( users.to[Array].reduceLeft(_ max _) - users.to[Array].reduceLeft(_ min _) ) + 1
+val n = ( movies.to[Array].reduceLeft(_ max _) - movies.to[Array].reduceLeft(_ min _) ) + 1
+val n_iterations = 10
+// var ratingArr = ratings.to[Array]
 // Check Pivot Q
 def createPivotQ( uid: Int, mid: Int ): Double = {
   var res: Double = 0.0 
@@ -51,6 +68,14 @@ def createPivotQ( uid: Int, mid: Int ): Double = {
     }
   }
   return res
+}
+var Q = new ListBuffer[Array[Double]]()
+for( i <- users.to[Array].reduceLeft(_ min _) to users.to[Array].reduceLeft(_ max _) ){
+  var lineArr = new Array[Double](n)
+  for( j <- movies.to[Array].reduceLeft(_ min _) to movies.to[Array].reduceLeft(_ max _) ){
+    lineArr(j-movies.to[Array].reduceLeft(_ min _)) = createPivotQ( i, j )
+  }
+  Q += lineArr
 }
 // Check Pivot W
 def createPivotW( uid: Int, mid: Int ): Double = {
@@ -62,19 +87,45 @@ def createPivotW( uid: Int, mid: Int ): Double = {
   }
   return res
 }
+var W = new ListBuffer[Array[Double]]()
+for( i <- users.to[Array].reduceLeft(_ min _) to users.to[Array].reduceLeft(_ max _) ){
+  var lineArr = new Array[Double](n)
+  for( j <- movies.to[Array].reduceLeft(_ min _) to movies.to[Array].reduceLeft(_ max _) ){
+    lineArr(j-movies.to[Array].reduceLeft(_ min _)) = createPivotW( i, j )
+  }
+  W += lineArr
+}
+
 // Parameter
-val lambda_ = 0.1
-val n_factors = 100
-val m = ( users.to[Array].reduceLeft(_ max _) - users.to[Array].reduceLeft(_ min _) ) + 1
-val n = ( movies.to[Array].reduceLeft(_ max _) - movies.to[Array].reduceLeft(_ min _) ) + 1
-val n_iterations = 20
+
 // *** //
 
+// Matrix X
+var X = new ListBuffer[Array[Double]]()
+for( i <- 0 to m - 1 ){
+  var lineArr = new Array[Double](n_factors)
+  for( j <- 0 to n_factors - 1 ){
+    lineArr(j) = ( Math.random * 5 )
+  }
+  X += lineArr
+}
+// println("999999999999")
+print_readable(X)
+// Matrix 
+var Y = new ListBuffer[Array[Double]]()
+for( i <- 0 to n_factors - 1 ){
+  var lineArr = new Array[Double](n)
+  for( j <- 0 to  n - 1 ){
+    lineArr(j) = ( Math.random * 5 )
+  }
+  Y += lineArr
+}
+print_readable(Y)
 // Transpose
-import scala.collection.mutable.ListBuffer
-val test = new ListBuffer[Array[Double]]()
-test += Array(4.0,7.0,2.0,1.0)
-test += Array(3.0,9.0,8.0,6.0)
+// import scala.collection.mutable.ListBuffer
+// val test = new ListBuffer[Array[Double]]()
+// test += Array(4.0,7.0,2.0,1.0)
+// test += Array(3.0,9.0,8.0,6.0)
 def transe( arr: ListBuffer[Array[Double]] ): ListBuffer[Array[Double]] = {
     val temp = arr
     var res = new ListBuffer[Array[Double]]
@@ -88,13 +139,13 @@ def transe( arr: ListBuffer[Array[Double]] ): ListBuffer[Array[Double]] = {
     return res
 }
 // Mult
-import scala.collection.mutable.ListBuffer
-val test1 = new ListBuffer[Array[Double]]()
-test1 += Array(1.0,0.0)
-test1 += Array(0.0,1.0)
-val test2 = new ListBuffer[Array[Double]]()
-test2 += Array(4.0,1.0)
-test2 += Array(2.0,2.0)
+// import scala.collection.mutable.ListBuffer
+// val test1 = new ListBuffer[Array[Double]]()
+// test1 += Array(1.0,0.0)
+// test1 += Array(0.0,1.0)
+// val test2 = new ListBuffer[Array[Double]]()
+// test2 += Array(4.0,1.0)
+// test2 += Array(2.0,2.0)
 def sub_mul( i: Int, j: Int, arr1: ListBuffer[Array[Double]], arr2: ListBuffer[Array[Double]] ): Double = {
     var res = 0.0
     for( k <- 0 to arr1.apply(0).length - 1 ){
@@ -116,12 +167,12 @@ def mult( arr1: ListBuffer[Array[Double]], arr2: ListBuffer[Array[Double]] ): Li
     return res
 }
 // Inverse 
-import scala.collection.mutable.ListBuffer
+// import scala.collection.mutable.ListBuffer
 import scala.util.control._
-val test1 = new ListBuffer[Array[Double]]()
-test1 += Array(7.0,2.0,1.0)
-test1 += Array(0.0,3.0,-1.0)
-test1 += Array(-3.0,4.0,-2.0)
+// val test1 = new ListBuffer[Array[Double]]()
+// test1 += Array(7.0,2.0,1.0)
+// test1 += Array(0.0,3.0,-1.0)
+// test1 += Array(-3.0,4.0,-2.0)
 def assign( posI: Int, posJ: Int, value: Double, arr: ListBuffer[Array[Double]]): ListBuffer[Array[Double]] = {
    var res = new ListBuffer[Array[Double]]
     for( i <- 0 to arr.length - 1 ){
@@ -210,13 +261,13 @@ def inverse( arr1: ListBuffer[Array[Double]] ): ListBuffer[Array[Double]] = {
     return I
 }
 // Solve
-import scala.collection.mutable.ListBuffer
-val test1 = new ListBuffer[Array[Double]]()
-test1 += Array(7.0,5.0)
-test1 += Array(3.0,-2.0)
-val test2 = new ListBuffer[Array[Double]]()
-test2 += Array(3.0)
-test2 += Array(22.0)
+// import scala.collection.mutable.ListBuffer
+// val test1 = new ListBuffer[Array[Double]]()
+// test1 += Array(7.0,5.0)
+// test1 += Array(3.0,-2.0)
+// val test2 = new ListBuffer[Array[Double]]()
+// test2 += Array(3.0)
+// test2 += Array(22.0)
 def solve( arr1: ListBuffer[Array[Double]], arr2: ListBuffer[Array[Double]] ): ListBuffer[Array[Double]] = {
     var res = new ListBuffer[Array[Double]]
     var in_arr1 = inverse(arr1)
@@ -225,7 +276,7 @@ def solve( arr1: ListBuffer[Array[Double]], arr2: ListBuffer[Array[Double]] ): L
 }
 // Eye
 import scala.collection.mutable.ListBuffer
-def eyeStar( lambda: Int, n: Int): ListBuffer[Array[Double]] = {
+def eyeStar( lambda: Double, n: Int): ListBuffer[Array[Double]] = {
     var res = new ListBuffer[Array[Double]]
     for( i <- 0 to n - 1 ){
         var lineArr = new Array[Double](n)
@@ -288,20 +339,160 @@ def sumR( arr: ListBuffer[Array[Double]]): Double = {
     return res
 }
 // Get error
-def get_error( Q: ListBuffer[Array[Double]], X: ListBuffer[Array[Double]], Y: ListBuffer[Array[Double]], W: ListBuffer[Array[Double]] ): Double = {
-    var xy = mult(X,Y)
-    var qxy = minus(Q,xy)
-    var wqxy = star(W,xy)
-    var wqxy2 = start(wqxy,wqxy)
+def get_error( q: ListBuffer[Array[Double]], x: ListBuffer[Array[Double]], y: ListBuffer[Array[Double]], w: ListBuffer[Array[Double]] ): Double = {
+    var xy = mult(x,y)
+    var qxy = minus(q,xy)
+    var wqxy = star(w,qxy)
+    var wqxy2 = star(wqxy,wqxy)
+    println("++++++")
+    print_readable(x)
+    print(y)
+    println(xy)
+    println(sumR(wqxy2))
+    println("++++++")
     return sumR(wqxy2)
 }   
 var errors = new ListBuffer[Double]()
 for( ii <- 0 to n_iterations - 1 ){
+    // var x1 = plus( mult( Y, transe(Y) ) ,  eyeStar( lambda_, n_factors ) )
+    // var x2 = mult( Y, transe(Q) )
+    // var x3 = solve(x1,x2)
+    // X = transe(x3)
+    
+    println("111111111111111")
+    print_readable(X)
     X = transe( solve( plus( mult( Y, transe(Y) ) ,  eyeStar( lambda_, n_factors ) ), mult( Y, transe(Q) ) ))
+    // var y1 = plus( mult( transe(X), X ) ,  eyeStar( lambda_, n_factors ) )
+    // var y2 = mult( transe(X), Q )
+    // var y3 = solve(y1,y2)
+    // Y = y3
     Y = solve(  plus( mult( transe(X), X ) ,  eyeStar( lambda_, n_factors ) ), mult( transe(X), Q ) )
     // if(ii%100==0){
-        println(ii+"iteration is completed")
+    println(ii+"iteration is completed")
     // }
-    errors += get_error( Q, X, Y, W )
+    var error = get_error(Q, X, Y, W)
+    println("Error of rated movies: "+error)
+    errors += error
 }
-var Q_hat = mult( X, Y )
+// var Q_hat = mult( X, Y )
+// val movies_id = new ListBuffer[Int]()
+// val movies_title = new ListBuffer[String]()
+// class Movie( mid: Int, mti: String){
+//     movies_id += mid
+//     movies_title += mti
+//     def getMovieId(): Int = mid
+//     def getMovieTitle(): String = mti
+//     override def toString: String = s" movie_id: $mid , movie_title: $mti"  
+// }
+// import scala.io.Source
+// import scala.collection.mutable.ListBuffer
+// val filename2 = "data/movies.csv"
+// var movieZ = new ListBuffer[Movie]()
+// var numLine1: Int = 0
+// for (line <- Source.fromFile(filename2).getLines()) {
+//   numLine1 += 1
+//   if(numLine1>1) {
+//     val movie_id = line.split(",")(0).toInt 
+//     val movie_title = line.split(",")(1).toString
+//     var temp = new Movie(movie_id,movie_title)
+//     movieZ += temp
+
+//   }
+// }
+// // Min
+// def minimum( arr1: ListBuffer[Array[Double]] ): Double = {
+//     var res = arr1.apply(0).apply(0)
+//     for( i <- 0 to arr1.length - 1 ){
+//         for( j <- 0 to arr1.apply(0).length - 1 ){
+//             if( arr1.apply(i).apply(j) < res ){
+//                 res = arr1.apply(i).apply(j)
+//             }
+//         }
+//     }
+//     return res 
+// }
+// // Max
+// def maximum( arr1: ListBuffer[Array[Double]] ): Double = {
+//     var res = arr1.apply(0).apply(0)
+//     for( i <- 0 to arr1.length - 1 ){
+//         for( j <- 0 to arr1.apply(0).length - 1 ){
+//             if( arr1.apply(i).apply(j) > res ){
+//                 res = arr1.apply(i).apply(j)
+//             }
+//         }
+//     }
+//     return res 
+// }
+// // minus_const
+// def minus_const( const: Double, arr1: ListBuffer[Array[Double]] ): ListBuffer[Array[Double]] = {
+//     var res = new ListBuffer[Array[Double]]
+//     for( i <- 0 to arr1.length -1 ){
+//         var lineArr = new Array[Double](arr1.apply(0).length)
+//         for( j <- 0 to arr1.apply(0).length - 1 ){
+//             lineArr(j) = arr1.apply(i).apply(j) - const
+//         }
+//         res += lineArr
+//     }
+//     return res
+// }
+// // const_minus
+// def const_minus( const: Double, arr1: ListBuffer[Array[Double]] ): ListBuffer[Array[Double]] = {
+//     var res = new ListBuffer[Array[Double]]
+//     for( i <- 0 to arr1.length -1 ){
+//         var lineArr = new Array[Double](arr1.apply(0).length)
+//         for( j <- 0 to arr1.apply(0).length - 1 ){
+//             lineArr(j) = const - arr1.apply(i).apply(j)
+//         }
+//         res += lineArr
+//     }
+//     return res
+// }
+// // star_const
+// def star_const( const: Double, arr1: ListBuffer[Array[Double]] ): ListBuffer[Array[Double]] = {
+//     var res = new ListBuffer[Array[Double]]
+//     for( i <- 0 to arr1.length -1 ){
+//         var lineArr = new Array[Double](arr1.apply(0).length)
+//         for( j <- 0 to arr1.apply(0).length - 1 ){
+//             lineArr(j) = arr1.apply(i).apply(j) * const
+//         }
+//         res += lineArr
+//     }
+//     return res
+// }
+// // argmax
+// import scala.collection.mutable.ListBuffer
+// val test1 = new ListBuffer[Array[Double]]()
+// test1 += Array(0.0,1.0,2.0)
+// test1 += Array(3.0,4.0,5.0)
+// def indiceMax( arr1: Array[Double] ): Int = {
+//     var index = 0
+//     var max = arr1.apply(0)
+//     for( i <- 0 to arr1.length - 1 ){
+//         if(arr1.apply(i)>max){
+//             max = arr1.apply(i)
+//             index = i
+//         }
+//     }   
+//     return index
+// }
+// def argmax( arr1: ListBuffer[Array[Double]] ): ListBuffer[Array[Double]] = {
+//     var res = new ListBuffer[Array[Double]]
+//     var lineArr = new Array[Double](arr1.length)
+//     for( j <- 0 to arr1.length - 1 ){
+//         lineArr(j) = indiceMax(arr1.apply(j))
+//     }
+//     res += lineArr
+//     return res
+// }
+// def print_recommendation( W: ListBuffer[Array[Double]], Q: ListBuffer[Array[Double]], Qhat: ListBuffer[Array[Double]]){
+//     var Qhat1 = minus_const(minimum(Qhat),Qhat)
+//     var Qhat2 = star_const(5.0/maximum(Qhat1),Qhat1)
+//     var m_id = argmax( star( const_minus( 5, Qhat2 ), W) )
+//     for( i <- 1 to m ){
+//         for( j <- m_id ){
+//             println(i+"--"+j)
+//         }
+//     }
+// }
+// print_recommendation( W, Q , Q_hat)
+
